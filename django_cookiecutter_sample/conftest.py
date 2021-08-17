dad054818beba0bec40e2fab20f8bce678e61641
Oerd Cukalla <rosaccu@gmail.com>
2021-08-17 16:06:00 +0200

    Just generated

---
+++ django_cookiecutter_sample/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_cookiecutter_sample.users.models import User
+from django_cookiecutter_sample.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
